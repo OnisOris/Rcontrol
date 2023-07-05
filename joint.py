99b0dbf3b01b42f8c18fe563174e27d8d1fbda53
OnisOris <55390311+OnisOris@users.noreply.github.com>
2023-07-05 23:18:31 +0300

    Commone structure of program

---
+++ joint.py
@@ -0,0 +1,9 @@
+class Joint:
+    def __init__(self, name=None, limit_of_movement=None, length=None):
+        self.name = name
+        self.limit_of_movement = limit_of_movement
+        self.length = length
+
+    
+
+
